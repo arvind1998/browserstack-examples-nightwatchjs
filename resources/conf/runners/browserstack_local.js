5d467d6e27f9d6141e2c5678322ef7b69cf70918
Arvind Ramadurai <arvind.r@browserstack.com>
2021-09-01 22:05:39 +0530

    changed local testing

--- resources/conf/runners/browserstack_local.js
+++ resources/conf/runners/browserstack_local.js
@@ -7,50 +7,56 @@ require("dotenv").config();
 
 let bs_local;
 
-try {
-	const accessKey =
-		process.env.BROWSERSTACK_ACCESS_KEY || Capabilities.access_key;
-	const pathToNightwatchExecutable =
-		"./node_modules/nightwatch/bin/nightwatch";
-	require.main.filename = pathToNightwatchExecutable;
-	process.mainModule.filename = pathToNightwatchExecutable;
-	// Code to start browserstack local before start of test
-	console.log("Connecting local");
-	Nightwatch.bs_local = bs_local = new browserstack.Local();
-	let localIdentifier;
-	if (Capabilities.local_args["localIdentifier"]) {
-		localIdentifier = Capabilities.local_args["localIdentifier"];
-	} else if (Capabilities.local_args["local-identifier"]) {
-		localIdentifier = Capabilities.local_args["local-identifier"];
-	}
-	localIdentifier =
-		process.env.BROWSERSTACK_LOCAL_IDENTIFIER ||
-		localIdentifier ||
-		new Date().getTime();
-	process.env.BROWSERSTACK_LOCAL_IDENTIFIER = localIdentifier;
-	bs_local.start(
-		{
-			key: accessKey,
-			...Capabilities.local_args,
-			localIdentifier: localIdentifier,
-		},
-		function (error) {
-			if (error) throw error;
+const runTests = function (error) {
+	if (error) throw error;
 
-			console.log("Connected. Now testing...");
-			Nightwatch.cli(function (argv) {
-				Nightwatch.CliRunner(argv)
-					.setup(null, function () {
-						// Code to stop browserstack local after end of parallel test
-						bs_local.stop(function () {});
-					})
-					.runTests(function () {
-						// Code to stop browserstack local after end of single test
-						bs_local.stop(function () {});
-					});
+	console.log("Connected. Now testing...");
+	Nightwatch.cli(function (argv) {
+		Nightwatch.CliRunner(argv)
+			.setup(null, function () {
+				// Code to stop browserstack local after end of parallel test
+				bs_local.stop(function () {});
+			})
+			.runTests(function () {
+				// Code to stop browserstack local after end of single test
+				bs_local.stop(function () {});
 			});
+	});
+}
+
+try {
+	if(!process.env.BROWSERSTACK_LOCAL){
+		const accessKey =
+			process.env.BROWSERSTACK_ACCESS_KEY || Capabilities.access_key;
+		const pathToNightwatchExecutable =
+			"./node_modules/nightwatch/bin/nightwatch";
+		require.main.filename = pathToNightwatchExecutable;
+		process.mainModule.filename = pathToNightwatchExecutable;
+		// Code to start browserstack local before start of test
+		console.log("Connecting local");
+		Nightwatch.bs_local = bs_local = new browserstack.Local();
+		let localIdentifier;
+		if (Capabilities.local_args["localIdentifier"]) {
+			localIdentifier = Capabilities.local_args["localIdentifier"];
+		} else if (Capabilities.local_args["local-identifier"]) {
+			localIdentifier = Capabilities.local_args["local-identifier"];
 		}
-	);
+		localIdentifier =
+			process.env.BROWSERSTACK_LOCAL_IDENTIFIER ||
+			localIdentifier ||
+			new Date().getTime();
+		process.env.BROWSERSTACK_LOCAL_IDENTIFIER = localIdentifier;
+		bs_local.start(
+			{
+				key: accessKey,
+				...Capabilities.local_args,
+				localIdentifier: localIdentifier,
+			},
+			runTests
+		);
+	} else {
+		runTests(undefined)
+	}
 } catch (ex) {
 	console.log("There was an error while starting the test runner:\n\n");
 	process.stderr.write(ex.stack + "\n");
